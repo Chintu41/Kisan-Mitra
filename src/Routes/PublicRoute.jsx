57bcadc2f00aadc6ce49a2e47f05a1709aa3e943
Starboy7704 <saipraneethmokalla@gmail.com>
2026-01-02 22:17:19 +0530

    feat: added loading spinner

--- src/Routes/PublicRoute.jsx
+++ src/Routes/PublicRoute.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import AppwriteAccount from "../Appwrite/Account.Services";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
 
 function PublicRoute({ children }) {
   const [user, setUser] = useState(null);
@@ -23,7 +25,19 @@ function PublicRoute({ children }) {
   }, []);
 
   if (isCheckingUser) {
-    return <div>Checking authentication...</div>;
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-linear-to-b from-green-50 to-emerald-100">
+        <Button
+          variant="outline"
+          disabled
+          size="sm"
+          className="flex items-center gap-2"
+        >
+          <Spinner />
+          Please wait
+        </Button>
+      </div>
+    );
   }
 
   if (user) {
